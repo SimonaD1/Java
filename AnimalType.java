d39f69de3d4492593c8e7ee133e14e79835f4650
SimonaDimitrovska <dimitrovskasimona1@gmail.com>
2019-02-25 14:47:55 +0100

    Add files via upload

---
+++ AnimalType.java
@@ -0,0 +1,24 @@
+package ConditionalStatementsLab;
+
+import java.util.Scanner;
+
+public class AnimalType {
+    public static void main(String[] args) {
+        Scanner scanner = new Scanner(System.in);
+        String animal = scanner.nextLine();
+        switch (animal)
+        {
+            case "dog":
+                System.out.println("mammal"); break;
+            case "crocodile":
+                System.out.println("reptile");
+            case "tortoise":
+                System.out.println("reptile");
+            case "snake":
+                System.out.println("reptile");
+                break;
+            default:
+                System.out.println("unknown");
+        }
+    }
+}
